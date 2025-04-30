0343493587e9d4c3d0fa0ee5c203e17ff3be91d2
Project-Faust <pentecost.cody.lane@gmail.com>
2025-04-30 16:49:59 -0400

    added header functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer"
 
 export const metadata: Metadata = {
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="">
         <div className="min-h-screen flex flex-col">
-          {/* <Header /> */}
+          <Header />
           <main className="flex-grow">
             {children}
           </main>
